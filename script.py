5a5b02372050e7874c2c6f496cf8b0d214505594
Kevin <kwang11@stuy.edu>
2019-06-14 22:12:02 -0400

    whoops

--- script.py
+++ script.py
@@ -135,7 +135,7 @@ def run(filename):
         screen = new_screen()
         zbuffer = new_zbuffer()
         tmp = []
-        step_3d = 20
+        step_3d = 100
         consts = ''
         coords = []
         coords1 = []
